655101a32b55fc2f983081cc4b52b25afaf8e3c8
rrhythmsharma <rrhythmsharma@gmail.com>
2022-01-02 18:25:03 +0530

    Added: mock api routes[routes are publicly open]

--- src/routes/mockapi/mock.route.js
+++ src/routes/mockapi/mock.route.js
@@ -6,6 +6,15 @@ const mockController = require('../../controllers/mock.controller');
 
 const router = express.Router();
 
-router.route('/:prefix/:projectId/:resource').get(validate(mockValidation.getMockApiData), mockController.getMockApiData);
+router
+  .route('/:prefix/:projectId/:resource')
+  .get(validate(mockValidation.getMockApiData), mockController.getMockApiData)
+  .post(validate(mockValidation.appendMockApiDataById), mockController.appendMockApiDataById);
+
+router
+  .route('/:prefix/:projectId/:resource/:id')
+  .get(validate(mockValidation.getMockApiDataById), mockController.getMockApiDataById)
+  .put(validate(mockValidation.getAndCreateMockApiDataById), mockController.getAndCreateMockApiDataById)
+  .delete(validate(mockValidation.deletMockApiDataById), mockController.deletMockApiDataById);
 
 module.exports = router;
