fa51dc2ba935fba8e5b0a9edde61ac78b25a4a7d
rrhythmsharma <rrhythmsharma@gmail.com>
2022-01-02 18:24:24 +0530

    Updated: mock controllers

--- src/controllers/mock.controller.js
+++ src/controllers/mock.controller.js
@@ -1,34 +1,43 @@
-// const httpStatus = require('http-status');
-// const ApiError = require('../utils/ApiError');
 const pick = require('../utils/pick');
 const catchAsync = require('../utils/catchAsync');
 const { mockService } = require('../services');
 
 const getMockApiData = catchAsync(async (req, res) => {
   const params = pick(req.params, ['prefix', 'projectId', 'resource']);
-  // req.params.route
-  // ['api/v1', 'project id', 'resourse name']
-  // const { route } = req.params;
-
-  // let params = route.split('/');
-  // const resourseName = params[params.length - 1];
-  // const projectId = params[params.length - 2];
-
-  // let prefix = '';
-  // if (params.length >= 2) {
-  //   params.forEach((item, index) => {
-  //     if (index < params.length - 2) {
-  //       prefix += item;
-  //     }
-  //   });
-  // }
-
-  // params = [prefix, projectId, resourseName];
 
   const result = await mockService.queryMockData(params);
   res.send(result);
 });
 
+const getMockApiDataById = catchAsync(async (req, res) => {
+  const params = pick(req.params, ['prefix', 'projectId', 'resource', 'id']);
+  const result = await mockService.queryMockDataByResponseObjectId(params);
+  res.send(result);
+});
+
+const deletMockApiDataById = catchAsync(async (req, res) => {
+  const params = pick(req.params, ['prefix', 'projectId', 'resource', 'id']);
+  const result = await mockService.deleteMockDataByResponseObjectId(params);
+  res.send(result);
+});
+
+const appendMockApiDataById = catchAsync(async (req, res) => {
+  const params = pick(req.params, ['prefix', 'projectId', 'resource']);
+  const result = await mockService.appendMockDataByResponseObjectId(params);
+  res.send(result);
+});
+
+const getAndCreateMockApiDataById = catchAsync(async (req, res) => {
+  const { body } = req;
+  const params = pick(req.params, ['prefix', 'projectId', 'resource', 'id']);
+  const result = await mockService.getAndCreateMockDataByResponseObjectId(body, params);
+  res.send(result);
+});
+
 module.exports = {
   getMockApiData,
+  getMockApiDataById,
+  getAndCreateMockApiDataById,
+  deletMockApiDataById,
+  appendMockApiDataById,
 };
